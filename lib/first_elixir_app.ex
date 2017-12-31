3f01255ad8b65389d15c848a3994b98f3ba0d1c3
sourav sarkar <souravsrkr254@gmail.com>
2017-12-31 23:21:22 +0530

    Minor Syntax Error and Using Cond

--- lib/first_elixir_app.ex
+++ lib/first_elixir_app.ex
@@ -46,4 +46,23 @@ defmodule FirstElixirApp do
     end
 
     sqrt(2 * gravity * distance)
+  end
+
+  def fall_gravity_velocity_type(planet, distance) when distance >= 0 do
+    gravity = case planet do
+      :earth -> 9.8
+      :moon  -> 1.6
+      :mars  -> 3.71
+    end
+
+    velocity = sqrt(2 * gravity * distance)
+
+    cond do
+      velocity == 0 -> :stable
+      velocity < 5  -> :slow
+      velocity >= 5 and velocity < 10 -> :moving
+      velocity >= 10 and velocity < 20 -> :fast
+      velocity >= 20 -> :flash
+    end
+  end
 end
